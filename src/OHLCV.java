43fc1fa4371b335c55be3f2433efbdd7f1acc53a
jkravets1 <61854432+jkravets1@users.noreply.github.com>
2020-04-05 23:07:47 -0400

    Add files via upload
    Fixed CSV, added treemap of OHCLV, added current quote

---
+++ src/OHLCV.java
@@ -0,0 +1,24 @@
+
+public class OHLCV {
+
+	/**
+	 * Class to hold open, high, low, close, volume data in treemap
+	 */
+
+	public Double open;
+	public Double high;
+	public Double low;
+	public Double close;
+	public Double volume;
+
+	public OHLCV( Double open, Double high,Double low,Double close,Double volume) {
+		
+		this.open= open; 
+		this.high= high; 
+		this.low= low; 
+		this.close= close;
+		this.volume=volume;
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/Stock.java
+++ src/Stock.java
@@ -1,31 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 
 
 /**
- * STOCK CLASS:
- * 
- * This class stores a stock's ticker, price, and historical data
- * 
+ * A class to store a stock's ticker, price, and historical data
  * @author kravetsj
  *
  */
 public class Stock {
 
-private static final TreeMap<LocalDate, Double> TreeMap = null;
+
 private String ticker;
 private String csv;
-private Object currentprice;
-private String quote;
+private double quote;
 private TreeMap datahistory;
 
 	/**
-	 * STOCK METHOD:
 	 * Stock object with stock's ticker, price, and historical data
 	 * @param ticker
+	 * @throws FileNotFoundException 
 	 */
 	
-	public Stock(String ticker) {
+	public Stock(String ticker) throws FileNotFoundException {
 		// TODO Auto-generated constructor stub
 		
 		DataPull t= new DataPull();
@@ -40,18 +43,51 @@ private TreeMap datahistory;
 	
 	
 	/**
-	 * GETTREEMAP METHOD:
 	 * Tree map to store data from stock csv
 	 * @param ticker
+	 * @throws FileNotFoundException 
 	 */
-	private TreeMap<LocalDate, Double> getTreemap(String csv, String Ticker) {
+	private TreeMap<LocalDate, OHLCV> getTreemap(String csv, String Ticker) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		DataPull t= new DataPull();
-		t.getcsv(csv);
+		t.getcsv(ticker);
+		File data= new File(csv);
+		Scanner fileReader = new Scanner(data);
+		fileReader.nextLine(); 
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d");
 		
-		return TreeMap;
+		TreeMap datahistory =new TreeMap<>();
+       
+		
+		int iteration = 0;
+        while (fileReader.hasNextLine()) {
+        	
+        	
+			String line = fileReader.nextLine();
+			String[] lineComponents = line.split(",");
+			
+			if (!lineComponents[0].equals("timestamp")) {
+				
+			LocalDate localDate = LocalDate.parse(lineComponents[0], formatter);
+			double open = Double.parseDouble(lineComponents[1]);
+			double high = Double.parseDouble(lineComponents[2]);
+			double low = Double.parseDouble(lineComponents[3]);
+			double close = Double.parseDouble(lineComponents[4]);
+			double volume = Double.parseDouble(lineComponents[5]);
+			
+			OHLCV ohlcv= new OHLCV( open,  high, low, close, volume);
+			datahistory.put(localDate, ohlcv);
+
+			}
+			
+			
+		}
+		return datahistory;
 	}
 
 
-}	
+
 	
+	
+
+}

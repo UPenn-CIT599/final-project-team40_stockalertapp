7a39ca425fa539694da9590aea4ce10f68929998
Robert Stanton <rstant@seas.upenn.edu>
2020-04-11 16:42:11 -0400

    updated button functionality, added time frame adjust buttons, and reformatted text

--- src/StockDetailButton.java
+++ src/StockDetailButton.java
@@ -1,5 +1,9 @@
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 /**
@@ -12,6 +16,7 @@ public class StockDetailButton extends JButton{
     private Color backGroundColor = Color.DARK_GRAY;
     private Color foreGroundColor = Color.WHITE;
     private Color hoverColor = Color.GRAY;
+    private Font tickerFont;
     
     /**
      * Constructs the JButton with a Stock object attribute as well as setting the background and foreground colors.
@@ -19,12 +24,15 @@ public class StockDetailButton extends JButton{
      */
     public StockDetailButton(Stock s) {
         stock = s;
+        tickerFont = new Font(Font.SANS_SERIF, Font.PLAIN, 16);
+        
+        this.setFont(tickerFont);
         this.setText(stock.getTicker());
         this.setBackground(backGroundColor);
         this.setForeground(foreGroundColor);
         this.setBorderPainted(false);
         this.setOpaque(true);
-        
+        this.addMouseListener(new MouseEventActions());
     }
     
     /**
@@ -42,5 +50,37 @@ public class StockDetailButton extends JButton{
      */
     public Stock getStock() {
         return stock;
+    }
+    
+    private class MouseEventActions implements MouseListener {
+        
+        
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            ((JButton) e.getSource()).setForeground(new Color(222, 180, 132, 100));
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            ((JButton) e.getSource()).setForeground(Color.WHITE);
+            
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            ((JButton)e.getSource()).setBackground(Color.GRAY);
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            ((JButton) e.getSource()).setBackground(Color.DARK_GRAY); 
+            
+        }
+        
     }
 }

befdc3021a9f602642ee552fa14cefde1b2c212e
Robert Stanton <rstant@seas.upenn.edu>
2020-04-15 19:22:17 -0400

    updates to Stock class for camel case

--- src/Stock.java
+++ src/Stock.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
@@ -19,142 +18,174 @@ import java.nio.file.Paths;
 /**
  * A class to store a stock's ticker, price, and historical data
  * 
- * @author kravetsj
+ * @author Joseph Kravets
  *
  */
-public class Stock implements Serializable{
+public class Stock {
 
-	/**
+    private String ticker;
+    private String csv;
+    private double quote;
+    private TreeMap dataHistory;
+
+    /**
+     * Stock object with stock's ticker, price, and historical data
+     * 
+     * @param ticker
+     * @throws FileNotFoundException
+     * @throws InterruptedException
+     */
+
+    public Stock(String ticker) throws FileNotFoundException, InterruptedException {
+
+	DataPull data = new DataPull();
+
+	this.ticker = ticker;
+	this.csv = ticker + ".csv";
+
+	try {
+	    this.quote = data.getCurrentQuote(ticker);
+	    this.dataHistory = getTreeMap(csv, ticker);
+
+	} catch (java.time.format.DateTimeParseException e) {
+	    System.out.print("waiting 30 seconds due to api rate limit");
+	    TimeUnit.SECONDS.sleep(30);
+	    this.dataHistory = getTreeMap(csv, ticker);
+	    this.quote = data.getCurrentQuote(ticker);
+	}
+    }
+
+    /**
+     * Tree map to store data from stock csv
+     * 
+     * @param ticker
+     * @throws FileNotFoundException
+     * @throws InterruptedException
+     */
+    private TreeMap<LocalDate, OHLCV> getTreeMap(String csv, String ticker)
+	    throws FileNotFoundException, InterruptedException {
+
+	System.gc();
+
+	DataPull dataPull = new DataPull();
+	dataPull.getCsv(ticker);
+	File data = new File(csv);
+	Scanner fileReader = new Scanner(data);
+	fileReader.nextLine();
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d");
+
+	TreeMap dataHistory = new TreeMap<>();
+
+	while (fileReader.hasNextLine()) {
+
+	    String line = fileReader.nextLine();
+	    String[] lineComponents = line.split(",");
+
+	    if (!lineComponents[0].equals("timestamp")) {
+
+		LocalDate localDate = LocalDate.parse(lineComponents[0], formatter);
+		double open = Double.parseDouble(lineComponents[1]);
+		double high = Double.parseDouble(lineComponents[2]);
+		double low = Double.parseDouble(lineComponents[3]);
+		double close = Double.parseDouble(lineComponents[4]);
+		double volume = Double.parseDouble(lineComponents[5]);
+
+		OHLCV ohlcv = new OHLCV(open, high, low, close, volume);
+		dataHistory.put(localDate, ohlcv);
+
+	    }
+	}
+	fileReader.close();
+
+	try {
+	    System.gc();
+	    Files.deleteIfExists(Paths.get((csv)));
+
+	} catch (NoSuchFileException e) {
+	    System.out.println("No such file/directory exists");
+	} catch (IOException e) {
+
+	    e.printStackTrace();
+	}
+
+	return dataHistory;
+    }
+
+    /**
+     * Getters and setters
+     * 
+     * @return
+     */
+
+    public String getTicker() {
+	return ticker;
+    }
+
+    public void setTicker(String ticker) {
+	this.ticker = ticker;
+    }
+
+    public String getCsv() {
+	return csv;
+    }
+
+    public void setCsv(String csv) {
+	this.csv = csv;
+    }
+
+    public double getQuote() {
+	return quote;
+    }
+
+    public void setQuote(double quote) {
+	this.quote = quote;
+    }
+
+    public TreeMap getdataHistory() {
+	return dataHistory;
+    }
+
+    public void setdataHistory(TreeMap dataHistory) {
+	this.dataHistory = dataHistory;
+    }
+
+    /*
+     * main for testing
      * 
      */
-    private static final long serialVersionUID = -1995220666246658729L;
-    private String ticker;
-	private String csv;
-	private double quote;
-	private TreeMap datahistory;
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+	// TODO Auto-generated method stub
 
-	/**
-	 * Stock object with stock's ticker, price, and historical data
-	 * 
-	 * @param ticker
-	 * @throws FileNotFoundException
-	 * @throws InterruptedException
-	 */
+	Stock t;
 
-	public Stock(String ticker) throws FileNotFoundException, InterruptedException {
-		// TODO Auto-generated constructor stub
+	t = new Stock("SLB");
 
-		DataPull t = new DataPull();
+	// get all entries
+	Set<Map.Entry<LocalDate, OHLCV>> entries = t.dataHistory.entrySet();
 
-		this.ticker = ticker;
-		this.csv = ticker + ".csv";
-		// this.quote= t.getcurrentquote(ticker);
-
-		try {
-			this.quote = t.getcurrentquote(ticker);
-			this.datahistory = getTreemap(csv, ticker);
-
-		} catch (java.time.format.DateTimeParseException e) {
-			System.out.print("waiting 30 seconds due to api rate limit");
-			TimeUnit.SECONDS.sleep(30);
-			this.datahistory = getTreemap(csv, ticker);
-			this.quote = t.getcurrentquote(ticker);
-		}
+	// using for loop
+	for (Map.Entry<LocalDate, OHLCV> entry : entries) {
+	    System.out.println(entry.getKey() + " open " + entry.getValue().open + " high " + entry.getValue().high
+		    + " low " + entry.getValue().low + " close " + entry.getValue().close + " volume "
+		    + entry.getValue().volume);
 	}
 
-	/**
-	 * Tree map to store data from stock csv
-	 * 
-	 * @param ticker
-	 * @throws FileNotFoundException
-	 * @throws InterruptedException
-	 */
-	private TreeMap<LocalDate, OHLCV> getTreemap(String csv, String Ticker)
-			throws FileNotFoundException, InterruptedException {
-		// TODO Auto-generated method stub
+	System.out.println(t.quote);
 
-		System.gc();
+	t = new Stock("UPRO");
 
-		DataPull t = new DataPull();
-		t.getcsv(ticker);
-		File data = new File(csv);
-		Scanner fileReader = new Scanner(data);
-		fileReader.nextLine();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d");
+	// get all entries
+	Set<Map.Entry<LocalDate, OHLCV>> entries1 = t.dataHistory.entrySet();
 
-		TreeMap datahistory = new TreeMap<>();
-
-		while (fileReader.hasNextLine()) {
-
-			String line = fileReader.nextLine();
-			String[] lineComponents = line.split(",");
-
-			if (!lineComponents[0].equals("timestamp")) {
-
-				LocalDate localDate = LocalDate.parse(lineComponents[0], formatter);
-				double open = Double.parseDouble(lineComponents[1]);
-				double high = Double.parseDouble(lineComponents[2]);
-				double low = Double.parseDouble(lineComponents[3]);
-				double close = Double.parseDouble(lineComponents[4]);
-				double volume = Double.parseDouble(lineComponents[5]);
-
-				OHLCV ohlcv = new OHLCV(open, high, low, close, volume);
-				datahistory.put(localDate, ohlcv);
-
-			}
-		}
-		fileReader.close();
-
-		try {
-			System.gc();
-			Files.deleteIfExists(Paths.get((csv)));
-			// System.out.println("Deleted old "+csv+ "file");
-		} catch (NoSuchFileException e) {
-			System.out.println("No such file/directory exists");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return datahistory;
+	// using for loop
+	for (Map.Entry<LocalDate, OHLCV> entry : entries1) {
+	    System.out.println(entry.getKey() + " open " + entry.getValue().open + " high " + entry.getValue().high
+		    + " low " + entry.getValue().low + " close " + entry.getValue().close + " volume "
+		    + entry.getValue().volume);
 	}
 
-	/**
-	 * Getters and setters
-	 * 
-	 * @return
-	 */
+	System.out.println(t.quote);
 
-	public String getTicker() {
-		return ticker;
-	}
-
-	public void setTicker(String ticker) {
-		this.ticker = ticker;
-	}
-
-	public String getCsv() {
-		return csv;
-	}
-
-	public void setCsv(String csv) {
-		this.csv = csv;
-	}
-
-	public double getQuote() {
-		return quote;
-	}
-
-	public void setQuote(double quote) {
-		this.quote = quote;
-	}
-
-	public TreeMap getDatahistory() {
-		return datahistory;
-	}
-
-	public void setDatahistory(TreeMap datahistory) {
-		this.datahistory = datahistory;
-	}
+    }
 
 }

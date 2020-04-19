f8c93ca915c0163694128123c1934caad312fa3c
tiffchoi <tiffchoi@seas.upenn.edu>
2020-04-19 11:52:31 -0400

    Updated Alerts class

--- src/Alerts.java
+++ src/Alerts.java
@@ -1,7 +1,7 @@
 /***
  * ALERTS CLASS:
  * 
- * This class maintains the list of stocks with their respective chosen alerts.
+ * This class calculates the Alerts. 
  * 
  * @author Tiffany Choi
  *
@@ -10,15 +10,16 @@
 public class Alerts {
 	
 	/**
-	 * BELOWPRICEALERT METHOD: 
-	 * This method alerts the user if the price is below or equal to a certain number.
-	 * @return true: if the price is below the number
-	 * @return false: if the price is above the number
+	 * BELOWALERT METHOD: 
+	 * This method alerts the user if the indicator is below or equal to the saved alert.\
+	 * 
+	 * @return true: if the indicator is below the number
+	 * @return false: if the indicator is equal or above the number
 	 */
 	
-	public boolean belowPriceAlert(double price, double pricealert) {
+	public boolean belowPriceAlert(double indicator, double savedalert) {
 		
-		if (price < pricealert) {
+		if (indicator < savedalert) {
 			return true;
 		} 
 
@@ -27,153 +28,19 @@ public class Alerts {
 	}
 		
 	/**
-	 * ABOVEPRICEALERT METHOD: 
-	 * This method alerts the user if the price is above a certain number.
-	 * @return true: if the price is above the number
-	 * @return false: if the price is below the number
+	 * ABOVEALERT METHOD: 
+	 * This method alerts the user if the indicator is above the saved alert.
+	 * 
+	 * (Includes the SMA Crossover Alert and EMA Crossover Alert)
+	 * @return true: if the indicator is equal or above the number
+	 * @return false: if the indicator is below the number
 	 */
 	
-	public boolean abovePriceAlert(double price, double pricealert) {
+	public boolean abovePriceAlert(double indicator, double savedalert) {
 		
-		if (price >= pricealert) {
+		if (indicator >= savedalert) {
 			return true;
 		}
 		
 		return false;
 	}
-		
-	/**
-	 * SMACROSSOVERALERT METHOD: 
-	 * This method alerts the user if the SMAs crossover
-	 * @return true: if the SMA crossed over
-	 * @return false: if the SMA did not cross over
-	 */
-	
-	public boolean smaCrossoverAlert(double sma, double smaalert) {
-		
-		if (sma >= smaalert) {
-			return true;
-		}
-		
-		return false;
-	}
-	
-	
-	/**
-	 * EMACROSSOVERALERT METHOD: 
-	 * This method alerts the user if the EMAs crossover
-	 * @return true: if the EMA crossed over
-	 * @return false: if the EMA did not cross over
-	 */
-	
-	public boolean emaCrossoverAlert(double ema, double emaalert) {
-		
-		if (ema >= emaalert) {
-			return true;
-		}
-		
-		return false;
-	}
-	
-	
-	/**
-	 * BELOWRSIALERT METHOD: 
-	 * This method alerts the user if the RSI is below a certain number.
-	 * @return true: if the RSI is below the number
-	 * @return false: if the RSI is above the number
-	 */
-	
-	public boolean belowRSIAlert(double rsi, double rsialert) {
-		
-		if (rsi < rsialert) {
-			return true;
-		} 
-		
-		return false;
-		
-	}
-		
-	/**
-	 * ABOVERSIALERT METHOD: 
-	 * This method alerts the user if the RSI is above a certain number.
-	 * @return true: if the RSI is above the number
-	 * @return false: if the RSI is below the number
-	 */
-	
-	public boolean aboveRSIAlert(double rsi, double rsialert) {
-		
-		if (rsi >= rsialert) {
-			return true;
-		} 
-		
-		return false;
-	}
-	
-	
-	/**
-	 * BELOWMACDALERT METHOD: 
-	 * This method alerts the user if the MACD is below a certain number.
-	 * @return true: if the MACD is below the number
-	 * @return false: if the MACD is above the number
-	 */
-	
-	public boolean belowMACDAlert(double macd, double macdalert) {
-		
-		if (macd < macdalert) {
-			return true;
-		} 
-		
-		return false;
-		
-	}
-		
-	/**
-	 * ABOVEMACDALERT METHOD: 
-	 * This method alerts the user if the MACD is above a certain number.
-	 * @return true: if the MACD is above the number
-	 * @return false: if the MACD is below the number
-	 */
-	
-	public boolean aboveMACDAlert(double macd, double macdalert) {
-		
-		if (macd >= macdalert) {
-			return true;
-		} 
-		
-		return false;
-	}
-	
-	/**
-	 * BELOWOBVALERT METHOD: 
-	 * This method alerts the user if the OBV is below a certain number.
-	 * @return true: if the OBV is below the number
-	 * @return false: if the OBV is above the number
-	 */
-	
-	public boolean belowOBVAlert(double obv, double obvalert) {
-		
-		if (obv < obvalert) {
-			return true;
-		} 
-		
-		return false;
-		
-	}
-		
-	/**
-	 * ABOVEOBVALERT METHOD: 
-	 * This method alerts the user if the OBV is above a certain number.
-	 * @return true: if the OBV is above the number
-	 * @return false: if the OBV is below the number
-	 */
-	
-	public boolean aboveOBVAlert(double obv, double obvalert) {
-		
-		if (obv >= obvalert) {
-			return true;
-		}
-		
-		return false;
-	}
-	
-}

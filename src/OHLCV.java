c663606d747461e876a309e08692ea4f2f216461
Robert Stanton <rstant@seas.upenn.edu>
2020-04-23 20:33:20 -0400

    added serializable to OHLCV

--- src/OHLCV.java
+++ src/OHLCV.java
@@ -1,26 +1,27 @@
+import java.io.Serializable;
 
-/**
- * Class to hold open, high, low, close, volume data in TreeMap
- * 
- * @author Joseph Kravets
- *
- */
-public class OHLCV {
+public class OHLCV implements Serializable {
 
-    public Double open;
-    public Double high;
-    public Double low;
-    public Double close;
-    public Double volume;
+	/**
+     * 
+     */
+    private static final long serialVersionUID = 8773796337479006667L;
+    /**
+	 * Class to hold open, high, low, close, volume data in treemap
+	 */
 
-    public OHLCV(Double open, Double high, Double low, Double close, Double volume) {
+	public Double open;
+	public Double high;
+	public Double low;
+	public Double close;
+	public Double volume;
 
-	this.open = open;
-	this.high = high;
-	this.low = low;
-	this.close = close;
-	this.volume = volume;
-
-    }
-
+	public OHLCV( Double open, Double high,Double low,Double close,Double volume) {
+		
+		this.open= open; 
+		this.high= high; 
+		this.low= low; 
+		this.close= close;
+		this.volume=volume;
+	}
 }
